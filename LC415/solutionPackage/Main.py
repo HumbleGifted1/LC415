4a1a4829eb2f07d8cfceaed50bcf79cef086ff9a
mahalymarcelin <mahalymarcelin@localhost>
2022-11-17 08:42:25 -0500

    first

---
+++ LC415/solutionPackage/Main.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 17, 2022
+
+@author: mahalymarcelin
+'''
+
+# entry point test code; test solution class for leetcode problem 415.
+# solution provided by Professor
+
+from solutionPackage.Solution import *
+
+mySolution = Solution()
+
+result = mySolution.addStrings("69", "420")
+print(result)
